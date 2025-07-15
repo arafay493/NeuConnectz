a090b7635b0df713b4a8fdebd4227ea45b9f8bd2
SufiyanAK <sufiyankhattak14@gmail.com>
2025-07-15 18:09:09 +0500

    Refactor AppLayOut component to use separate CollapsedNavbar and ExpandedNavbar components for improved navigation handling and code organization.

--- src/components/app-layout/app-layout.tsx
+++ src/components/app-layout/app-layout.tsx
@@ -13,31 +13,23 @@ import NextImage from 'next/image';
 import {
   AppShell,
   AppShellHeader,
-  AppShellNavbar,
   AppShellMain,
   Group,
-  NavLink,
   Avatar,
   Text,
   ActionIcon,
   Stack,
-  Image,
-  Divider,
 } from '@mantine/core';
 import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 import {
-  IconLogout,
   IconBell,
-  IconChevronLeft,
-  IconChevronRight
 } from '@tabler/icons-react';
 import { useAppDispatch, useAppSelector } from '@/redux/store';
-import { DrawerRoute } from "@/types/route-types";
 import { routes, drawerRoutes, authenticatedRoutes } from '@/constants/routes';
-// import { localAssets } from '@/lib/file-paths/file-paths';
-import { logout } from '@/constants/logout';
 import { customStyles } from '@/styles/custom-theme';
 import { localAssets } from '@/lib/file-paths/file-paths';
+import CollapsedNavbar from './collapsed-navbar';
+import ExpandedNavbar from './expanded-navbar';
 
 const AppLayOut = ({ children }: { children: ReactNode }) => {
 
@@ -73,46 +65,6 @@ const AppLayOut = ({ children }: { children: ReactNode }) => {
     setActiveTab(activeRouteIndex >= 0 ? activeRouteIndex : -1);
   }, [pathName]);
 
-  // Note: Link component for navigation...!
-  const renderNavLink = (item: DrawerRoute, index: number) => (
-    <NavLink
-      href={item.route}
-      key={index}
-      component="a"
-      leftSection={item?.icon}
-      label={collapsed ? null : item?.label}
-      variant="light"
-      px={collapsed ? customStyles.deviceSize.sm : customStyles.deviceSize.md}
-      py={customStyles.deviceSize.sm}
-      color={activeTab === index ? customStyles.colors._1B59F8 : customStyles.colors._4D4D4D}
-      active={activeTab === index}
-      onClick={() => setActiveTab(index)}
-      w={collapsed ? 'fit-content' : '100%'}
-      style={{
-        textTransform: 'capitalize',
-        borderRadius: '10px',
-        transition: 'all 0.3s ease',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: collapsed ? 'center' : 'flex-start',
-        margin: collapsed ? '0 auto' : '0',
-      }}
-      styles={{
-        label: {
-          fontSize: 16,
-          fontWeight: 500,
-          transition: 'opacity 0.3s ease',
-          opacity: collapsed ? 0 : 1,
-        },
-        root: {
-          justifyContent: collapsed ? 'center' : 'flex-start',
-          width: collapsed ? 'fit-content' : '100%',
-          transition: 'all 0.3s ease',
-        },
-      }}
-    />
-  );
-
   return (
     <AppShell
       padding={customStyles.deviceSize.md}
@@ -129,7 +81,7 @@ const AppLayOut = ({ children }: { children: ReactNode }) => {
         px={24}
         style={{
           display: authenticatedRoutes.includes(pathName) || pathName.startsWith(authenticatedRoutes[10] as string) ? 'block' : 'none',
-          transition: 'margin-left 0.3s ease', // ✨ smooth movement
+          // transition: 'margin-left 0.3s ease', // ✨ smooth movement
         }}
       >
         <Group h='100%' align='center' justify='end'>
@@ -174,105 +126,21 @@ const AppLayOut = ({ children }: { children: ReactNode }) => {
       </AppShellHeader>
 
       {/* Note: Navbar section */}
-      <AppShellNavbar
-        w={collapsed ? 120 : 300}
-        bg={customStyles.colors.white}
-        h='100%'
-        pos='fixed'
-        top='0'
-        left='0'
-        zIndex={1000}
-        style={{
-          transition: 'width 0.3s ease', // Smoother transition duration
-          display: authenticatedRoutes.includes(pathName) || pathName.startsWith(authenticatedRoutes[10] as string) ? 'block' : 'none',
-        }}
-
-      >
-        <Stack h='100%' gap={0} >
-          <Group
-            h={80}
-            justify="space-between"
-            align="center"
-            px={customStyles.deviceSize.sm}
-            py={customStyles.deviceSize.sm}
-            style={{ cursor: 'pointer' }}
-            wrap='nowrap'
-          >
-            <Image
-              src={collapsed ? localAssets.logo_sm : localAssets.newLogo}
-              alt="Logo"
-              component={NextImage}
-              h={25}
-              w='auto'
-            />
-            <ActionIcon
-              variant="filled"
-              color={customStyles.colors._1B59F8}
-              size={25}
-              onClick={isMobile ? toggle : () => setCollapsed(!collapsed)}
-              style={{
-                '&:hover': {
-                  backgroundColor: '#e7f5ff'
-                }
-              }}
-            >
-              {
-                collapsed ? <IconChevronRight size={20} /> : <IconChevronLeft size={20} />
-              }
-            </ActionIcon>
-          </Group>
-          <Divider mx={26} />
-          <Stack
-            h='100%'
-            justify='space-around'
-            px={customStyles.deviceSize.sm}
-          >
-            <Stack
-              // mt={32}
-              gap={customStyles.deviceSize.md}
-              style={{
-                transition: 'all 0.3s ease',
-              }}
-            >
-              {drawerRoutes.map((item, index) => renderNavLink(item, index))}
-            </Stack>
-            <NavLink
-              component="a"
-              leftSection={<IconLogout size={24} />}
-              label={collapsed ? null : 'Logout'}
-              variant="light"
-              px={collapsed ? customStyles.deviceSize.sm : customStyles.deviceSize.md}
-              py={customStyles.deviceSize.sm}
-              bg='#ED1C241A'
-              color='#ED1C24'
-              active
-              onClick={() => logout("Log Out Success", "You have logged out successfully")}
-              w={collapsed ? 'fit-content' : '100%'}
-              style={{
-                borderRadius: '10px',
-                transition: 'all 0.3s ease',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: collapsed ? 'center' : 'flex-start',
-                margin: collapsed ? '0 auto' : '0',
-              }}
-              styles={{
-                label: {
-                  fontSize: 16,
-                  fontWeight: 500,
-                  transition: 'opacity 0.3s ease',
-                  opacity: collapsed ? 0 : 1,
-                },
-                root: {
-                  justifyContent: collapsed ? 'center' : 'flex-start',
-                  width: collapsed ? 'fit-content' : '100%',
-                  transition: 'all 0.3s ease',
-                },
-              }}
-            />
-          </Stack>
-        </Stack>
-      </AppShellNavbar>
+      {collapsed ? (
+        <CollapsedNavbar
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          setCollapsed={setCollapsed}
+          toggle={toggle}
+        />
+      ) : (
+        <ExpandedNavbar
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          setCollapsed={setCollapsed}
+          toggle={toggle}
+        />
+      )}
 
       {/* Note: Components section */}
       <AppShellMain style={{
