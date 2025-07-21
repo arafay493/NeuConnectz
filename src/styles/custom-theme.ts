28322e8cb0da8fdc67f5389e3469235289c2d32b
SufiyanAK <sufiyankhattak14@gmail.com>
2025-07-21 12:58:16 +0500

    Revamp login screen or better UI

--- src/styles/custom-theme.ts
+++ src/styles/custom-theme.ts
@@ -15,6 +15,7 @@ const customStyles: CustomStyles = {
         _4A4A4A: "#4A4A4A",
         _F5F7FA: "#F5F7FA",
         _ECECEC: "#ECECEC",
+        _F8F9FA: "#F8F9FA",
         _1B59F81A: "#1B59F81A",
         evenTableColor: "#E1E7EC80",
         tableRowBorderColor: "#E1E7EC",
